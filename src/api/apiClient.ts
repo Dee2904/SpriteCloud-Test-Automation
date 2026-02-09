a545735de05e50ae09a25aa91904a3c8cd774abf
P <p@p.com>
2026-02-09 04:32:13 +0100

    temp change

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -11,6 +11,11 @@ export class ApiClient {
       baseURL: environment.fakeStoreBaseUrl,
       extraHTTPHeaders: {
         'Content-Type': 'application/json',
+         'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ' +
+      'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+      'Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'application/json',
       },
     })
   }

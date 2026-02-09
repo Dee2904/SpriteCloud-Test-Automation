1c42d38a0d1a5735cc7fbae0d1c688e2bf5f6f13
P <p@p.com>
2026-02-09 04:51:31 +0100

    change env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const environment = {
-  sauceBaseUrl: process.env.SAUCE_BASE_URL as string,
-  fakeStoreBaseUrl: process.env.FAKESTORE_BASE_URL as string,
+  sauceBaseUrl: process.env.SAUCE_BASE_URL || '',
+  fakeStoreBaseUrl: process.env.FAKESTORE_BASE_URL || '',
 }

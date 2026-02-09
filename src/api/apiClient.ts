b6c1fd1a0dec8ed3f963b0b7a700711a13ceb41d
P <p@p.com>
2026-02-09 05:11:11 +0100

    fix1

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -9,13 +9,18 @@ export class ApiClient {
     console.log('FAKESTORE_BASE_URL:', environment.fakeStoreBaseUrl)//debug log to verify the base URL being used
     this.context = await request.newContext({
       baseURL: environment.fakeStoreBaseUrl,
-      extraHTTPHeaders: {
-        'Content-Type': 'application/json',
-         'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) ' +
-      'AppleWebKit/537.36 (KHTML, like Gecko) ' +
-      'Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'application/json',
+       extraHTTPHeaders: {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      'Accept': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
       },
     })
   }
